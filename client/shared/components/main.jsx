eaf2ddddddf55958cb239867cf7816c5745ba652
Ro Min Swe <rominswe@gmail.com>
2025-12-09 03:53:18 +0800

    few tweak

--- client/shared/components/main.jsx
+++ client/shared/components/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './../../shared/styles/index_1.css'
+import './../../shared/styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
